b236775b8cba7173f955b2dbc124cdd086128c4b
kalpeshmistari <darshanmistari827@gmail.com>
2024-02-17 19:50:30 +0530

    [MODIFIED]:Darshan - UC-2 Calculate the Daily Wage for Employee

--- src/com/darshan/employeewagecomputation/EmployeeWageComputation.java
+++ src/com/darshan/employeewagecomputation/EmployeeWageComputation.java
@@ -5,6 +5,9 @@ public class EmployeeWageComputation {
 	public static void main(String[] args) {
 
 		int attendance = 0;
+		int dailyWage = 0;
+		final int WAGE_PER_HR = 20;
+		final int FULL_DAY_HR = 8;
 		
 		System.out.println("Welcome to Employee Wage Computation Program");
 		
@@ -19,6 +22,8 @@ public class EmployeeWageComputation {
 		else
 		{
 			System.out.println("Employee is Present");
+		    dailyWage = WAGE_PER_HR * FULL_DAY_HR;
+		    System.out.println("Employee Wage :"+dailyWage);
 		}
 	}
 
