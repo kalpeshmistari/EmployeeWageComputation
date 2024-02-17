3599247fdec237683e724b1819d27bb8eb83f77e
kalpeshmistari <darshanmistari827@gmail.com>
2024-02-18 00:05:34 +0530

    [MODIFIED]:Darshan UC-5 Calculate Wage for Month Using For Loop

--- src/com/darshan/employeewagecomputation/EmployeeWageComputation.java
+++ src/com/darshan/employeewagecomputation/EmployeeWageComputation.java
@@ -10,6 +10,7 @@ public class EmployeeWageComputation {
 		final int WAGE_PER_HR = 20;
 		final int PART_TIME_HR = 4;
 		final int FULL_TIME_HR = 8;
+		final int WORKING_DAYS = 20 ;
 		
 		System.out.println("Welcome to Employee Wage Computation Program");
 		
@@ -17,30 +18,40 @@ public class EmployeeWageComputation {
 		attendance = random.nextInt(9)%2;
 		
 		System.out.println( attendance );
-		
-		switch(attendance)
+		for(int i = 1; i <= WORKING_DAYS ; i++)
 		{
-		case 0:
-			System.out.println("Employee is Absent");
-			break;
-		case 1:
-			System.out.println("Employee is Present");
-			employee_Type = random.nextInt(9)%2;
+			System.out.println("Day :" + i);
 			
-			switch(employee_Type)
+			switch(attendance)
 			{
+			
 			case 0:
-				System.out.println("Employee is Part Time");
-				dailyWage = WAGE_PER_HR * PART_TIME_HR;
+				System.out.println("Employee is Absent");
 				break;
+				
 			case 1:
-				System.out.println("Employee is Full Time ");
-				dailyWage = WAGE_PER_HR * FULL_TIME_HR;
+				System.out.println("Employee is Present");
+				employee_Type = random.nextInt(9)%2;
+				
+				switch(employee_Type)
+				{
+				
+				case 0:
+					System.out.println("Employee is Part Time");
+					dailyWage = WAGE_PER_HR * PART_TIME_HR;
+					break;
+					
+				case 1:
+					System.out.println("Employee is Full Time ");
+					dailyWage = WAGE_PER_HR * FULL_TIME_HR;
+					break;
+					
+				}
+				
+				System.out.println("Employee Wage :" + dailyWage);
 				break;
-			}
-			System.out.println("Employee Wage :" + dailyWage);
-			break;
+		   }
+			System.out.println("--------------------------");
 		}
 	}
-
 }
