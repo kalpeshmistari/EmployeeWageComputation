f7a679389172fcbac3cea9d990efbc4ba1e760aa
kalpeshmistari <darshanmistari827@gmail.com>
2024-02-17 19:11:40 +0530

    [MODIFIED]:Darshan - UC-1 Completed for Employee Attendance

--- src/com/darshan/employeewagecomputation/EmployeeWageComputation.java
+++ src/com/darshan/employeewagecomputation/EmployeeWageComputation.java
@@ -1,10 +1,25 @@
 package com.darshan.employeewagecomputation;
-
+import java.util.Random;
 public class EmployeeWageComputation {
 
 	public static void main(String[] args) {
 
-		System.out.println("Welcome to Employee Wage Computation Program.");
+		int attendance = 0;
+		
+		System.out.println("Welcome to Employee Wage Computation Program");
+		
+		Random random = new Random();
+		attendance = random.nextInt(9)%2;
+		
+		System.out.println( attendance );
+		if(attendance == 0)
+		{
+			System.out.println("Employee is Absent");
+		}
+		else
+		{
+			System.out.println("Employee is Present");
+		}
 	}
 
 }
