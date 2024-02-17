57620c89ae866b0fb62320565bc0b0f691659b5d
kalpeshmistari <darshanmistari827@gmail.com>
2024-02-17 23:03:03 +0530

    [MODIFIED]:Darshan - UC-4 Refactored code from if_else to switch_case

--- src/com/darshan/employeewagecomputation/EmployeeWageComputation.java
+++ src/com/darshan/employeewagecomputation/EmployeeWageComputation.java
@@ -5,9 +5,8 @@ public class EmployeeWageComputation {
 	public static void main(String[] args) {
 
 		int attendance;
-		int dailyWage;
+		int dailyWage = 0;
 		int employee_Type;
-		
 		final int WAGE_PER_HR = 20;
 		final int PART_TIME_HR = 4;
 		final int FULL_TIME_HR = 8;
@@ -18,26 +17,29 @@ public class EmployeeWageComputation {
 		attendance = random.nextInt(9)%2;
 		
 		System.out.println( attendance );
-		if(attendance == 0)
+		
+		switch(attendance)
 		{
+		case 0:
 			System.out.println("Employee is Absent");
-		}
-		else
-		{
+			break;
+		case 1:
 			System.out.println("Employee is Present");
 			employee_Type = random.nextInt(9)%2;
-			if(employee_Type == 0)
+			
+			switch(employee_Type)
 			{
-				   System.out.println("Employee is Part Time");
-				   dailyWage = WAGE_PER_HR * PART_TIME_HR;
-				   
-			}
-			else
-			{
+			case 0:
+				System.out.println("Employee is Part Time");
+				dailyWage = WAGE_PER_HR * PART_TIME_HR;
+				break;
+			case 1:
 				System.out.println("Employee is Full Time ");
 				dailyWage = WAGE_PER_HR * FULL_TIME_HR;
+				break;
 			}
 			System.out.println("Employee Wage :" + dailyWage);
+			break;
 		}
 	}
 
